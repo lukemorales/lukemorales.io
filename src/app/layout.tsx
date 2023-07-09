8f9c5d7e75eb418df7959a9e327ca6c3bcc332c3
Luke Morales <lukemorales@live.com>
2023-07-09 15:56:05 -0300

    Improve root metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,11 +8,6 @@ type RootLayoutProps = React.PropsWithChildren;
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
-  title: 'lukemorales.io',
-  description: 'Software Engineer',
-};
-
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
@@ -24,3 +19,35 @@ export default function RootLayout({ children }: RootLayoutProps) {
     </html>
   );
 }
+
+export const metadata: Metadata = {
+  metadataBase: new URL('https://lukemorales.io'),
+  title: {
+    default: 'Luke Morales',
+    template: '%s | Luke Morales',
+  },
+  description: 'Software Engineer',
+  openGraph: {
+    title: 'Luke Morales',
+    description: 'Software Engineer',
+    url: 'https://lukemorales.io',
+    siteName: 'Luke Morales',
+    locale: 'en-US',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  twitter: {
+    title: 'Luke Morales',
+    card: 'summary_large_image',
+  },
+};
